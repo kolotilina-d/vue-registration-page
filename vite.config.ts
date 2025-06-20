11c0e2c6d3ef9235ff746263328a938c463fceb1
Darya Kolotilina <kolotilina.d@gmail.com>
2025-06-20 13:33:05 +0200

    run build & run deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import path from 'path'
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-// })
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/vue-registration-page/', 
   plugins: [vue()],
   resolve: {
     alias: {
